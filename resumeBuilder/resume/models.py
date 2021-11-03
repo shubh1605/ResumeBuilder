8f816da2e6b9159426bbe00d8809af3699cfbdfa
shubh0208 <62873923+shubh0208@users.noreply.github.com>
2021-11-04 00:21:49 +0530

    home page done
    Co-authored-by: kashish1211 <kashish1211@users.noreply.github.com>
    Co-authored-by: HarryPuttar <HarshitJain28@users.noreply.github.com>

--- resumeBuilder/resume/models.py
+++ resumeBuilder/resume/models.py
@@ -68,3 +68,12 @@ class Skill(models.Model):
     def __str__(self):
         return f'{self.resume.user.username} | skill: {self.skill_detail} : {self.resume.template}'
     
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length = 250)
+    message = models.TextField()
+
+    def __str__(self):
+        return f'{self.name}'
--- resumeBuilder/resume/views.py
+++ resumeBuilder/resume/views.py
@@ -1,61 +1,77 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from user.models import Profile
 from django.views.decorators.clickjacking import xframe_options_exempt
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import *
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'resume/home.html')
+	if request.method == 'POST':
+		("Inside post")
+		name = request.POST.get('name') # getting data from first_name input 
+		email = request.POST.get('email')  # getting data from last_name input
+		subject = request.POST.get('subject')
+		message = request.POST.get('message')
+		if name == '' or email=='' or subject == '' or message == '' :
+			return JsonResponse({'message': 'field'})
+		try:
+			contact = Contact(name=name, email=email, subject=subject, message=message)
+			contact.save()
+			return JsonResponse({'message': 'success'})
+		except:
+			return JsonResponse({'message': 'error'})
+		
+
+	
+	return render(request, 'resume/home.html')
 
 
 @xframe_options_exempt
 def testing(request):
-    return render(request, 'resume_templates/template3.html')
+	return render(request, 'resume_templates/template3.html')
 
 
 @login_required
 def forms(request):
 
-    if request.method == 'POST':
-        try:
-            educations = json.loads(request.POST.get('education'))
-            jobs = json.loads(request.POST.get('job')) 
-            skills = json.loads(request.POST.get('skill'))      
-            info = json.loads(request.POST.get('info'))
-        except:
-            print('HEHEHEHEHEH')
-        # print(educations,jobs,skills,info)
-        ctx = {}
-        loggedUser = request.user
-        resume = Resume(user=loggedUser, template="template 1")
-        resume.save()
-        basicInfo = BasicInformation(resume=resume, first_name=info[0][0], last_name=info[1][0], email=info[2][0], contact=info[3]
-                                     [0], address=info[4][0], profession=info[5][0], github=info[6][0], linkedin=info[7][0], about=info[8][0])
-        
-        for education in educations:
-            new_education = Education(resume = resume, degree = education[0], branch = education[1], university = education[2], passing_year = education[3], result = education[4])
-            new_education.save()
+	if request.method == 'POST':
+		try:
+			educations = json.loads(request.POST.get('education'))
+			jobs = json.loads(request.POST.get('job')) 
+			skills = json.loads(request.POST.get('skill'))      
+			info = json.loads(request.POST.get('info'))
+		except:
+			print(educations,jobs,skills)
+		ctx = {}
+		loggedUser = request.user
+		resume = Resume(user=loggedUser, template="template 1")
+		resume.save()
+		basicInfo = BasicInformation(resume=resume, first_name=info[0][0], last_name=info[1][0], email=info[2][0], contact=info[3]
+									 [0], address=info[4][0], profession=info[5][0], github=info[6][0], linkedin=info[7][0], about=info[8][0])
+		
+		for education in educations:
+			new_education = Education(resume = resume, degree = education[0], branch = education[1], university = education[2], passing_year = education[3], result = education[4])
+			new_education.save()
 
-        for job in jobs:
-            new_job = Experience(resume=resume, job_title = job[0], employer = job[1], start_date = job[2], end_date =job[3], description = job[4])
-            new_job.save()
+		for job in jobs:
+			new_job = Experience(resume=resume, job_title = job[0], employer = job[1], start_date = job[2], end_date =job[3], description = job[4])
+			new_job.save()
 
 
-        for skill in skills:
-            new_skill = Skill(resume=resume, skill_detail= skill[0], skill_level = skill[1])
-            new_skill.save()
+		for skill in skills:
+			new_skill = Skill(resume=resume, skill_detail= skill[0], skill_level = skill[1])
+			new_skill.save()
 
-        basicInfo.save()
+		basicInfo.save()
 
-        return HttpResponse(json.dumps(ctx), content_type='application/json')
+		return HttpResponse(json.dumps(ctx), content_type='application/json')
 
-    context = {
-        'resume_template': 'resume_templates/template1.html',
-        'user' : request.user,
-    }
-    return render(request, 'resume/forms.html')
+	context = {
+		'resume_template': 'resume_templates/template1.html',
+		'user' : request.user,
+	}
+	return render(request, 'resume/forms.html')
